41e6657d2a71f42ab0f3646b2ac37dd533362529
worldofgeese <59834693+worldofgeese@users.noreply.github.com>
2022-11-01 15:10:11 +0100

    Fixup

--- firmware/keyboards/blue_wizard/keymaps/default/keymap.cpp
+++ firmware/keyboards/blue_wizard/keymaps/default/keymap.cpp
@@ -32,9 +32,9 @@ std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
         // right hand
          KC_F9, KC_F10,  KC_F11,  KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, TG(L_NUM), KC_SPC,
          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
-         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_SLSH,
          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_BSLS, KC_RSFT,
               KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,
         // right thumb
         KC_LGUI, KC_RCTL,
